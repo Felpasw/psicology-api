cf12151e70671d8bdf69654b511626659b3cdd72
Felpasw <felipecavalcantelacerda@hotmail.com>
2024-11-09 15:43:40 -0300

    feat: crud de pacientes

--- src/facade/patients.ts
+++ src/facade/patients.ts
@@ -1,17 +1,24 @@
 
+import PatientModel from "../config/tableModels/patients"
+import dbo from '../dbo/base'
+
+
 const get = async object => {
- 
+    return await  dbo.get(PatientModel, object)
 }
 
 const insert = async object => {
+    return await  dbo.insert(PatientModel, object)
   
 }
 
 const update = async (object, id) => {
-  
+    console.log(id)
+    return await  dbo.update(PatientModel,  id, object)
 }
 
 const remove = async id => {
+    return await  dbo.remove(PatientModel, id)
  
 }
 
