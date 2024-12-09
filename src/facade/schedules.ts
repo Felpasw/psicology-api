67eca13d1cdbfb3674005906a7d3cd205c15bf26
Felipe Cavalcante Lacerda <92037342+Felpasw@users.noreply.github.com>
2024-12-09 08:16:58 -0300

    Inserido model de agenda e regras de validação

--- src/facade/schedules.ts
+++ src/facade/schedules.ts
@@ -1,21 +1,57 @@
 import dbo from '../dbo/base'
 import schedule from '../config/tableModels/schedule'
+import * as Yup from 'yup'
+import { scheduleSchema } from '../models/schedule'
+import UserModel from '../config/tableModels/user'
 
-
-const get = async object => {
-    return await  dbo.get(schedule, object)
+const get = async (object) => {
+  return await dbo.get(schedule, object)
 }
 
-const insert = async object => {
-    
-    return await  dbo.insert(schedule, object) 
+const insert = async (object) => {
+  try {
+    await scheduleSchema.validate(object, { abortEarly: false })
+  } catch (error) {
+    if (error instanceof Yup.ValidationError) {
+      const errors = error.inner.reduce((acc, err) => {
+        acc[err.path] = err.message
+        return acc
+      }, {})
+      console.log({ errors })
+
+      return { errors }
+    }
+    const { createdBy } = object
+    const response = await dbo.get(UserModel, { _id: createdBy })
+    console.log(object)
+    console.log(createdBy)
+
+    if (!response) {
+      return { errors: { createdBy: 'Usuário de criação inválido.' } }
+    }
+  }
+
+  return await dbo.insert(schedule, object)
 }
-const update = async (object, id) => {
-    return await  dbo.update(schedule,  id, object)
+const update = async (object, id: string) => {
+  try {
+    await scheduleSchema.validate(object, { abortEarly: false })
+  } catch (error) {
+    if (error instanceof Yup.ValidationError) {
+      const errors = error.inner.reduce((acc, err) => {
+        acc[err.path] = err.message
+        return acc
+      }, {})
+
+      return { errors }
+    }
+  }
+
+  return await dbo.update(schedule, id, object)
 }
 
 const remove = async (id) => {
-    return await dbo.remove(schedule, id)
+  return await dbo.remove(schedule, id)
 }
-  
-export  { get, insert, update, remove }
+
+export { get, insert, update, remove }
---
+++ src/models/schedule.ts
@@ -0,0 +1,37 @@
+import * as Yup from 'yup'
+
+const scheduleSchema = Yup.object().shape({
+  title: Yup.string().required('O título da consulta é obrigatório').typeError('O título deve ser uma string'),
+  description: Yup.string().nullable().typeError('A descrição deve ser uma string'),
+  date: Yup.date().required('A data da consulta é obrigatória').typeError('A data deve ser uma data válida'),
+  startTime: Yup.string()
+    .required('O horário de início é obrigatório')
+    .typeError('O horário de início deve ser uma string'),
+  endTime: Yup.string()
+    .nullable()
+    .typeError('O horário de término deve ser uma string')
+    .test('is-after-startTime', 'O horário de término deve ser posterior ao horário de início', function (endTime) {
+      const { startTime } = this.parent
+      if (!endTime || !startTime) return true
+      return endTime > startTime
+    })
+    .test('is-max-4-hours', 'O intervalo entre o início e o término deve ser no máximo de 4 horas', function (endTime) {
+      const { startTime } = this.parent
+      if (!endTime || !startTime) return true
+
+      const start = new Date(`1970-01-01T${startTime}:00Z`)
+      const end = new Date(`1970-01-01T${endTime}:00Z`)
+
+      const intervalInHours = (end.getTime() - start.getTime()) / (1000 * 60 * 60)
+      return intervalInHours <= 4
+    }),
+  location: Yup.string().nullable().typeError('O local deve ser uma string'),
+  status: Yup.mixed<'confirmed' | 'pending' | 'cancelled'>()
+    .required('O status é obrigatório')
+    .oneOf(['confirmed', 'pending', 'cancelled'], 'Status deve ser "confirmed", "pending" ou "cancelled"'),
+  createdBy: Yup.string()
+    .required('O ID do criador é obrigatório')
+    .typeError('O ID do criador deve ser uma string válida'),
+})
+
+export { scheduleSchema }
