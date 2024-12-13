9b9dc808fb4cf75b7b93f702e62a9934c5a8ef93
Felipe Cavalcante Lacerda <92037342+Felpasw@users.noreply.github.com>
2024-12-13 15:00:36 -0300

    Ajuste de exibição de itens por mês

--- src/dbo/dashboard.ts
+++ src/dbo/dashboard.ts
@@ -23,72 +23,11 @@ async function getMonthlyRevenue() {
       },
       {
         $project: {
-          year: '$_id.year',
-          month: '$_id.month',
           faturamento: { $multiply: ['$totalConsultas', 62.5] },
         },
       },
       {
         $sort: { month: 1 },
-      },
-      {
-        $group: {
-          _id: null,
-          faturamentoPorMes: { $push: { month: '$month', faturamento: '$faturamento' } },
-        },
-      },
-      {
-        $project: {
-          faturamentoPorMes: {
-            $concatArrays: [
-              [
-                { month: 1, faturamento: 0 },
-                { month: 2, faturamento: 0 },
-                { month: 3, faturamento: 0 },
-                { month: 4, faturamento: 0 },
-                { month: 5, faturamento: 0 },
-                { month: 6, faturamento: 0 },
-                { month: 7, faturamento: 0 },
-                { month: 8, faturamento: 0 },
-                { month: 9, faturamento: 0 },
-                { month: 10, faturamento: 0 },
-                { month: 11, faturamento: 0 },
-                { month: 12, faturamento: 0 },
-              ],
-              '$faturamentoPorMes',
-            ],
-          },
-        },
-      },
-      {
-        $project: {
-          faturamentoPorMes: {
-            $map: {
-              input: '$faturamentoPorMes',
-              as: 'mes',
-              in: {
-                month: '$$mes.month',
-                faturamento: {
-                  $ifNull: [
-                    {
-                      $arrayElemAt: [
-                        {
-                          $filter: {
-                            input: '$faturamentoPorMes',
-                            as: 'item',
-                            cond: { $eq: ['$$item.month', '$$mes.month'] },
-                          },
-                        },
-                        0,
-                      ],
-                    },
-                    { faturamento: 0 },
-                  ],
-                },
-              },
-            },
-          },
-        },
       },
     ])
     return result
